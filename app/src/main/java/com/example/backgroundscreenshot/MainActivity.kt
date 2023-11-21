aa61a781ac230452331fa1a8747c328efe2523f6
Meharunnisa <meharu641@gmail.com>
2023-11-21 17:19:38 +0500

    used priveapp-permissions to enforce the permissisons so we dont have to ask for them

--- app/src/main/java/com/example/backgroundscreenshot/MainActivity.kt
+++ app/src/main/java/com/example/backgroundscreenshot/MainActivity.kt
@@ -1,44 +1,79 @@
 package com.example.backgroundscreenshot
 
-import android.content.ComponentName
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.projection.MediaProjectionManager
+import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.backgroundscreenshot.services.ScreenshotService
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_MEDIA_PROJECTION = 1
+    private val REQUEST_CODE_SCREENSHOT= 1
+    private val mediaProjectionManager: MediaProjectionManager by lazy {
+        application.getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+    }
+    private lateinit var startServiceButton: Button
+    private lateinit var serviceIntent: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (!isAccessibilityServiceEnabled(this, ScreenshotService::class.java)) {
-            startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+        serviceIntent = Intent(this, ScreenshotService::class.java)
+            startActivityForResult(mediaProjectionManager.createScreenCaptureIntent(), REQUEST_CODE_SCREENSHOT)
+    }
+
+    private fun checkPermissions(): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            REQUEST_MEDIA_PROJECTION
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+            if ( resultCode == Activity.RESULT_OK) {
+                startScreenshotService(resultCode, data!!)
+            } else {
+                if (resultCode == Activity.RESULT_CANCELED) {
+                    Toast.makeText(this, "Screen Cast Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+
+    private fun startScreenshotService(resultCode: Int, data: Intent) {
+        val serviceIntent = Intent(this, ScreenshotService::class.java).apply {
+            putExtra("resultCode", resultCode)
+            putExtra("data", data)
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ContextCompat.startForegroundService(this, serviceIntent)
+        }
+        else{
+            startService(serviceIntent)
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        startService(Intent(this, ScreenshotService::class.java))
-    }
 
-    /*override fun onPause() {
-        super.onPause()
-
-        // Stop the screenshot service
-        stopService(Intent(this, ScreenshotService::class.java))
-    }*/
-
-    private fun isAccessibilityServiceEnabled(
-        context: Context,
-        accessibilityService: Class<*>
-    ): Boolean {
-        val expectedComponentName = ComponentName(context, accessibilityService)
-        val enabledServicesSetting = Settings.Secure.getString(
-            context.contentResolver,
-            Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES
-        )
-        return enabledServicesSetting?.contains(expectedComponentName.flattenToString()) == true
-    }
 }
--- app/src/main/java/com/example/backgroundscreenshot/services/ScreenshotService.kt
+++ app/src/main/java/com/example/backgroundscreenshot/services/ScreenshotService.kt
@@ -1,95 +1,233 @@
 package com.example.backgroundscreenshot.services
 
-import android.accessibilityservice.AccessibilityService
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo
 import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Rect
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.hardware.display.DisplayManager
+import android.hardware.display.VirtualDisplay
+import android.media.Image
+import android.media.ImageReader
+import android.media.MediaScannerConnection
+import android.media.projection.MediaProjection
+import android.media.projection.MediaProjectionManager
+import android.os.Build
+import android.os.Build.VERSION_CODES.O
 import android.os.Environment
 import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.provider.MediaStore
+import android.util.DisplayMetrics
 import android.util.Log
-import android.view.accessibility.AccessibilityEvent
-import android.view.accessibility.AccessibilityNodeInfo
+import android.util.SparseIntArray
+import android.view.Surface
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.example.backgroundscreenshot.MainActivity
+import com.example.backgroundscreenshot.R
+import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.FileOutputStream
-import java.io.IOException
+import java.nio.ByteBuffer
 
-class ScreenshotService : AccessibilityService() {
-    private lateinit var rootNode: AccessibilityNodeInfo
-    private lateinit var rect: Rect
-    private lateinit var bitmap: Bitmap
-    private lateinit var canvas: Canvas
-    private val handler = Handler()
+class ScreenshotService : Service() {
+    private lateinit var mediaProjectionManager: MediaProjectionManager
+    private lateinit var mediaProjection: MediaProjection
+    private lateinit var imageReader: ImageReader
+    private lateinit var virtualDisplay: VirtualDisplay
+    private lateinit var displayMetrics : DisplayMetrics
+    private val handler = Handler(Looper.getMainLooper())
+    private var screenDensity = 320
+    private val NOTIFICATION_ID = 1
+    private val NOTIFICATION_CHANNEL_ID = "ScreenshotServiceChannel"
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent) {
-        if (event.eventType == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-            takeScreenshot()
-        }
+    override fun onBind(intent: Intent): IBinder? {
+        return null
     }
 
-    private fun takeScreenshot() {
-        rootNode = rootInActiveWindow
-        rect = Rect()
-        rootNode.getBoundsInScreen(rect)
-        bitmap = Bitmap.createBitmap(rect.width(), rect.height(), Bitmap.Config.ARGB_8888)
-        canvas = Canvas(bitmap)
-        drawNodeRecursive(canvas, rootNode, rect)
-        saveScreenshotToFile(bitmap)
-    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-    private fun saveScreenshotToFile(bitmap: Bitmap) {
-        val screenshotFile = File(
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
-            "screenshot_${System.currentTimeMillis()}.png"
+        initRunningTipNotification()
+        val notification = buildNotification()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            startForeground(
+                NOTIFICATION_ID,
+                notification,
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PROJECTION
+            )
+        } else startForeground(
+            NOTIFICATION_ID,
+            notification
         )
+        if (intent != null) {
+            val resultCode = intent.getIntExtra("resultCode", 0)
+            val data = intent.getParcelableExtra<Intent>("data")
+            init(resultCode, data)
+        }
+        return START_NOT_STICKY
+    }
+
+    private fun buildNotification(): Notification {
+        val channelId =
+            createNotificationChannel("my_service", "Capture Service")
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags =
+                Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT and Intent.FLAG_ACTIVITY_SINGLE_TOP and Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+        val notification = notificationBuilder.setOngoing(true)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setPriority(NotificationCompat.PRIORITY_MIN)
+            .setContentText(
+                "Service is running"
+            )
+            .setContentIntent(pendingIntent)
+            .setCategory(Notification.CATEGORY_STATUS)
+            .build()
+        return notification
+    }
+
+
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val chan = if (Build.VERSION.SDK_INT >= O) {
+            NotificationChannel(
+                channelId,
+                channelName, NotificationManager.IMPORTANCE_NONE
+            )
+        } else {
+            return channelId
+        }
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+
+    private fun initRunningTipNotification() {
+        val builder: Notification.Builder
+        var notificationManager: NotificationManager
+        if (Build.VERSION.SDK_INT >= O) {
+            val channel = NotificationChannel(
+                "running",
+                "Running Notification",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            builder = Notification.Builder(this, channel.id)
+            notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        } else {
+            builder = Notification.Builder(this)
+        }
+
+        builder.setContentText("Running Notification")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+        notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        startForeground(100, builder.build())
+    }
+
+    private fun init(resultCode: Int, data: Intent?) {
+        mediaProjectionManager =
+            getSystemService(MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+        mediaProjection =
+            data?.let { mediaProjectionManager.getMediaProjection(resultCode, it) }!!
+        displayMetrics = DisplayMetrics()
+        (getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.getMetrics(displayMetrics)
+        imageReader = ImageReader.newInstance(displayMetrics.widthPixels, displayMetrics.heightPixels, PixelFormat.RGBA_8888, 2)
+        startCapture()
+    }
+
+    private fun startCapture() {
+        virtualDisplay = mediaProjection.createVirtualDisplay(
+            "ScreenshotService",
+            displayMetrics.widthPixels , displayMetrics.heightPixels, screenDensity,
+            DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+            imageReader.surface, null, null
+        )
+        captureScreenshot()
+        handler.postDelayed({
+            captureScreenshot()
+            virtualDisplay.release()
+            startCapture()
+        }, CAPTURE_INTERVAL.toLong())
+    }
+
+    private fun captureScreenshot() {
+        val image : Image = imageReader.acquireLatestImage()
+        if (image != null) {
+            imageToBitmap(image)
+            image.close()
+        }
+    }
+
+    private fun imageToBitmap(image: Image) {
         try {
-            FileOutputStream(screenshotFile).use { out ->
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+            val buffer: ByteBuffer = image.planes[0].buffer
+            val bytes = ByteArray(buffer.remaining())
+            buffer.get(bytes)
+
+            val filename = "${System.currentTimeMillis()}.jpg"
+            Log.d("FileName", "FileName: $filename")
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+            val imageFile = File(imagesDir, filename)
+            Log.e("FilePath", ":${imageFile.absolutePath}")
+
+            FileOutputStream(imageFile).use { outputStream ->
+                outputStream.write(bytes)
             }
-        } catch (e: IOException) {
+
+            MediaScannerConnection.scanFile(
+                this,
+                arrayOf(imageFile.absolutePath),
+                arrayOf("image/jpeg"),
+                null
+            )
+
+            showToast("Image saved: ${imageFile.absolutePath}")
+        } catch (e: Exception) {
+            Log.e("CaptureScreenshot", "Error saving image: ${e.message}")
             e.printStackTrace()
-        }
-        Log.d("FilePath", ":$screenshotFile")
-    }
-
-    private fun drawNodeRecursive(canvas: Canvas, node: AccessibilityNodeInfo, bounds: Rect) {
-        val nodeBounds = Rect()
-        node.getBoundsInScreen(nodeBounds)
-        if (nodeBounds.width() > 0 && nodeBounds.height() > 0) {
-            canvas.clipRect(bounds)
-            canvas.save()
-            canvas.translate(nodeBounds.left.toFloat(), nodeBounds.top.toFloat())
-            if (node.childCount > 0) {
-                for (i in 0 until node.childCount) {
-                    val childNode = node.getChild(i)
-                    drawNodeRecursive(canvas, childNode, bounds)
-                }
-            } /*else {
-                drawNode(canvas, node, nodeBounds)
-            }*/
-            canvas.restore()
+            showToast("Error saving image")
         }
     }
 
-/*    private fun drawNode(canvas: Canvas, node: AccessibilityNodeInfo, bounds: Rect) {
-        if (node.className == "android.view.View") {
-            val bitmap = Bitmap.createBitmap(bounds.width(), bounds.height(), Bitmap.Config.ARGB_8888)
-            canvas.drawBitmap(bitmap,bounds.left,bounds.top,null)//drawBitmap(node.window.decorHintBitmap, bounds.left, bounds.top, null)
+
+    private fun showToast(message: String) {
+        handler.post {
+            Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
         }
-    }*/
-
-    override fun onServiceConnected() {
-        super.onServiceConnected()
-        handler.postDelayed(screenshotRunnable, 60000) // 60,000 milliseconds = 1 minute
     }
 
-    override fun onInterrupt() {
-        // Implement if your service needs to handle interruptions
-    }
 
-    private val screenshotRunnable = object : Runnable {
-        override fun run() {
-            takeScreenshot()
-            handler.postDelayed(this, 60000)
+    companion object {
+        private const val CAPTURE_INTERVAL = 60 * 1000 // 1 minute
+        private val ORIENTATION = SparseIntArray()
+
+        init {
+            ORIENTATION.append(Surface.ROTATION_0, 90)
+            ORIENTATION.append(Surface.ROTATION_90, 0)
+            ORIENTATION.append(Surface.ROTATION_180, 270)
+            ORIENTATION.append(Surface.ROTATION_270, 180)
         }
     }
 }
+
